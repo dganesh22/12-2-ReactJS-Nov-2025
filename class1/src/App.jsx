d4d3b0bad3b27c1897bad8de7baac06023ad3d5f
dganesh22 <dganesh2022@gmail.com>
2025-11-19 12:13:12 +0530

    first class of react

---
+++ class1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// react functional Component
+function App(props) {
+  return (
+    <div>
+        <h1>Welcome to React JS</h1>
+    </div>
+  )
+}
+
+export default App
